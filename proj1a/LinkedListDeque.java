85e8d82ed59fe9fa7db4073609404006d497d815
olivia <zhouxuanolivia@gmail.com>
2021-02-02 11:23:56 +1000

    Link all the prev and next together

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,7 +1,7 @@
 public class LinkedListDeque<T> {
 
-    private class LinkedNode {
-        private T item;
+    public class LinkedNode {
+        public T item;
         private LinkedNode next;
         private LinkedNode prev;
 
@@ -13,21 +13,45 @@ public class LinkedListDeque<T> {
     }
 
     private T DEFAULT_ITEM;
-    private LinkedNode sentinel;
+    public LinkedNode sentinel;
+    public LinkedNode last;
+    public LinkedNode first;
     private int size;
 
     public LinkedListDeque() {
-        sentinel = new LinkedNode(DEFAULT_ITEM, null, null);
+        sentinel = new LinkedNode(DEFAULT_ITEM, sentinel, sentinel);
+        first = sentinel;
+        last = sentinel;
+
+        sentinel.prev = last;
+        sentinel.next = first;
+
+        first.prev = sentinel;
+        last.next = sentinel;
         size = 0;
     }
 
     public void addFirst(T item) {
-        sentinel.next = new LinkedNode(item, sentinel.next, sentinel.prev);
+        LinkedNode oldFirst = sentinel.next;
+        sentinel.next = new LinkedNode(item, oldFirst, sentinel);
+        first = sentinel.next;
+
+        first.prev = sentinel;
+        first.next = oldFirst;
+        oldFirst.prev = first;
+
+//        System.out.println("first item -> " + first.item);
         size++;
     }
 
     public void addLast(T item) {
-        sentinel.prev = new LinkedNode(item, sentinel.prev, sentinel.next);
+        LinkedNode oldLast = sentinel.prev;
+        sentinel.prev = new LinkedNode(item, sentinel, oldLast);
+        last = sentinel.prev;
+
+        last.next = sentinel;
+        last.prev = oldLast;
+        oldLast.next = last;
         size++;
     }
 
@@ -44,36 +68,48 @@ public class LinkedListDeque<T> {
 
     public void printDeque() {
         LinkedNode p = sentinel;
-        String res = "";
-        while (p.next != null) {
-            res += (p.next.item + " ");
+        String nextString = "";
+        String preString ="";
+        while (p.next != sentinel) {
+            nextString += (p.next.item + " ");
             p = p.next;
         }
-        p = sentinel;
-        while (p.prev != null) {
-//            System.out.println(p.prev.item);
-            res += (p.prev.item + " ");
-            p = p.prev;
-        }
-        System.out.println(res);
+//        p = sentinel;
+//        while (p.prev != null) {
+//            preString += (p.prev.item + " ");
+//            p = p.prev;
+//        }
+//        preString = preString.substring(0,preString.length()-1);
+//        while(preString.length()!=0){
+//            if (preString.substring(preString.length()-1)!=" "){
+//                nextString += preString.substring(preString.length()-1);
+//            }
+//            preString = preString.substring(0,preString.length()-1);
+//        }
+        System.out.println(nextString);
     }
 
     public T removeFirst() {
         size--;
-        T first = sentinel.next.item;
-        sentinel.next = sentinel.next.next;
-        return first;
+        T f = first.item;
+        first = first.next;
+        sentinel.next = first;
+        return f;
     }
 
     public T removeLast() {
         size--;
-        T last = sentinel.prev.item;
-        sentinel.prev = sentinel.prev.prev;
-        return last;
+        T l = last.item;
+        last = last.prev;
+        sentinel.prev = last;
+        return l;
     }
 
     /*Iteration*/
     public T get(int index) {
+        if(size -1 < index){
+            return null;
+        }
         return null;
     }
 
@@ -83,6 +119,7 @@ public class LinkedListDeque<T> {
 
     public static void main(String[] args) {
         LinkedListDeque<Integer> l = new LinkedListDeque();
+        l.addLast(8);
         l.addFirst(1);
         l.addLast(2);
         l.addFirst(3);
@@ -90,10 +127,17 @@ public class LinkedListDeque<T> {
         l.addLast(7);
         l.printDeque();
         System.out.println("1.The size of the list -> " + l.size());
-//        System.out.println(l.isEmpty());
-        System.out.println(l.removeFirst());
-        System.out.println(l.removeLast());
+        System.out.println("remove first item -> "+l.removeFirst());
+        System.out.println("remove last item -> "+l.removeLast());
         l.printDeque();
-        System.out.println("2.The size of the list -> " + l.size());
+//        System.out.println("______________________");
+//        System.out.println(l.sentinel.item);//null
+//        System.out.println(l.first.item); //3
+//        System.out.println(l.first.prev.item);//null
+//        System.out.println(l.first.prev.prev.item); //7
+//        System.out.println(l.first.prev.prev.prev.item);//5
+//        System.out.println(l.first.prev.prev.prev.prev.item);//1
+//        System.out.println(l.first.prev.prev.prev.prev.prev.item);//3
+
     }
 }
